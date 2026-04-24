075355b00f04c5254fe30c9199ee704098b7137d
Valentin Dhesdin <185408513+Soewyth@users.noreply.github.com>
2026-04-24 17:14:21 +0200

    feat: adding load data cifar

---
+++ src/cnn_cifar_10/data/load_data.py
@@ -0,0 +1,15 @@
+from torchvision import datasets
+
+
+def load_train_data(data_dir, transform=None):
+    """Loads the training data from the specified directory."""
+    return datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform
+    )
+
+
+def load_test_data(data_dir, transform=None):
+    """Loads the test data from the specified directory."""
+    return datasets.CIFAR10(
+        root=data_dir, train=False, download=True, transform=transform
+    )
