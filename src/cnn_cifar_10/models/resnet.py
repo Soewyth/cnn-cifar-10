cf6fa8be336091a8a8660df749750110603b4bb0
Valentin Dhesdin <185408513+Soewyth@users.noreply.github.com>
2026-04-26 17:44:43 +0200

    feat: freeze the parameters of layers for optimizer after

--- src/cnn_cifar_10/models/resnet.py
+++ src/cnn_cifar_10/models/resnet.py
@@ -4,11 +4,19 @@ from torch import nn
 
 def get_resnet18():
     """Get the ResNet-18 model."""
-    model = models.resnet18(
-        weights=models.ResNet18_Weights.DEFAULT
-    )  # Load the pre-trained modnel with default weights
-    in_features = model.fc.in_features  # Get nb of input features of the last fc layer
-    model.fc = nn.Linear(
-        in_features, 10
-    )  # take input feat and output 10 classes (for cifar-10)
+    # Load the pre-trained model with default weights
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
+    #  Freeze the pre-trained weights
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Replace the last fc layer with a new one that has 10 output classes (for CIFAR-10)
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, 10)
+
+    # Unfreeze the last fc layer (already unfrozen by default, but its explicitly set)
+    for param in model.fc.parameters():
+        param.requires_grad = True
+        
     return model
